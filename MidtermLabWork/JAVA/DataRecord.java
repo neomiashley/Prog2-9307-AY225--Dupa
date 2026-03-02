76bf7fa4767a31f0cf6a708a1a1bb5107bc1e884
neomiashley <biendupa24@gmail.com>
2026-03-02 22:57:34 +0800

    Midterm Lab Work

---
+++ MidtermLabWork/JAVA/DataRecord.java
@@ -0,0 +1,23 @@
+public class DataRecord {
+    private String title;
+    private String category;
+    private double sales;
+
+    public DataRecord(String title, String category, double sales) {
+        this.title = title;
+        this.category = category;
+        this.sales = sales;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getSales() {
+        return sales;
+    }
+}
